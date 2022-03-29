33041baaf1b7812fca724eb13120da4e08ad401f
Hasan F <hfidan006@gmail.com>
2022-03-29 20:07:07 +0200

    Initial commit

---
+++ src/test/java/pages/Appointment.java
@@ -0,0 +1,23 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class Appointment {
+
+    public Appointment() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/test/java/pages/Registration.java
@@ -0,0 +1,27 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class Registration {
+
+
+    public Registration() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
