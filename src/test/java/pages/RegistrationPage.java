904e197bb8ffe56036fa367d0e672a3b6a194f9c
BekirEnesAydin <bekirenesaydin@gmail.com>
2022-04-03 17:55:07 +0300

    TC01

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -13,6 +13,78 @@ public class RegistrationPage {
     }
 
 
+    //input[@name='ssn']
+    //div[text()='Your SSN is required.']
+    //div[text()='Your SSN is invalid']
+    //input[@class='is-touched is-dirty av-valid form-control']
+
+    @FindBy(xpath = "//input[@name='ssn']")
+    public WebElement ssnTextBox;
+
+    @FindBy(xpath = "//div[text()='Your SSN is required.']")
+    public WebElement ssnRequiredMessage;
+
+    @FindBy(xpath = "//div[text()='Your SSN is invalid']")
+    public WebElement ssnInvalidMessage;
+
+    @FindBy(xpath = "//input[@class='is-touched is-dirty av-valid form-control']")
+    public WebElement ssnValidConfirm;
+
+    //input[@name='firstName']
+    //div[text()='Your FirstName is required.']
+
+    @FindBy(xpath = "//input[@name='firstName']")
+    public WebElement firstNameTextBox;
+
+    @FindBy(xpath = "//div[text()='Your FirstName is required.']")
+    public WebElement firstNameRequiredMessage;
+
+    //input[@name='lastName']
+    //div[text()='Your LastName is required.']
+
+    @FindBy(xpath = "//input[@name='lastName']")
+    public WebElement lastNameTextBox;
+
+    @FindBy(xpath = "//div[text()='Your LastName is required.']")
+    public WebElement lastNameRequiredMessage;
+
+    //input[@name='username']
+    //div[text()='Your username is required.']
+    //div[text()='Your username is invalid.']
+
+    @FindBy(xpath = "//input[@name='username']")
+    public WebElement userNameTextBox;
+
+    @FindBy(xpath = "//div[text()='Your username is required.']")
+    public WebElement userNameRequiredMessage;
+
+    @FindBy(xpath = "//div[text()='Your username is invalid.']")
+    public WebElement usernameInvalidMessage;
+
+    //input[@name='email']
+    //div[text()='Your email is required.']
+    //div[text()='Your email is required to be at least 5 characters.']
+    //div[text()='This field is invalid']
+
+    @FindBy(xpath = "//input[@name='email']")
+    public WebElement emailTextBox;
+
+    @FindBy(xpath = "//div[text()='Your email is required.']")
+    public WebElement emailRequiredMessage;
+
+    @FindBy(xpath = "//div[text()='Your email is required to be at least 5 characters.']")
+    public WebElement emailCharacterMessage;
+
+    @FindBy(xpath = "//div[text()='This field is invalid']")
+    public WebElement emailInvalidMessage;
+
+
+
+
+
+
+
+
 
 
 
---
+++ src/test/resources/UI_Features/US001.feature
@@ -0,0 +1,16 @@
+Feature: US001_Kayıt olmak icin SSN, Firstname ve Lastname bilgileri girililebilmelidir.
+@us1
+  Scenario Outline:TC_01 Gecerli bir SSN, 3. ve 5. rakamdan sonra "-"  icermeli ve 9 rakamdan olusmalidir
+    When kullanici "medunnaUrl" sayfasina gider
+    Then sayfada kullanici ikonu altinda yer alan register butonuna tiklar
+    And SSN kutucuğuna tıklar ardından boş bırakır ve 'Your SSN is required.' uyarı mesajını görüntüler
+    Then kullanici SSN kutucuğuna sadece "<rakamlar>" girilebilmeli, "<harf>" ve "<özel karakter>" kabul edilmemeli, 'Your SSN is invalid' uyarı mesajı görüntülenmeli
+    And kullanici geçerli SSN için 3. ve 5. rakamdan sonra '-' karakteri eklemeli ve herhangi bir uyarı yazısı alınmamalıdır
+    Examples:
+      | rakamlar | harf | özel karakter |
+      | 1      |a  |-         |
+      |12      |ab |*/        |
+      |456     |asd|-*/       |
+      |8797    |fjkd|-=*-/    |
+      |594942  |dasas|*-/-\-  |
+      |56465*/ad| sad564/*|+*/--sad234|
