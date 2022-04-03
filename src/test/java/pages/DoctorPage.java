c82c3cde1f043e8059d6b0b2d191aa9d4bd9e10d
Ayşe <aysglgungor35@gmail.com>
2022-04-04 01:43:17 +0300

    0404

--- src/test/java/pages/DoctorPage.java
+++ src/test/java/pages/DoctorPage.java
@@ -1,27 +1,62 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 import utilities.Driver;
 
 public class DoctorPage {
 
     public DoctorPage() {
-        PageFactory.initElements(Driver.getDriver(),this);
+        PageFactory.initElements(Driver.getDriver(), this);
     }
 
 
+    @FindBy(xpath = "//span[normalize-space()='MY PAGES']")
+    public WebElement myPagesButton;
 
+    @FindBy(xpath = "//span[normalize-space()='My Appointments']")
+    public WebElement myAppointmentButton;
 
+    @FindBy(xpath = "//a[@class='btn btn-primary btn-sm']")
+    public WebElement myAppointmentEditButton;
 
+    @FindBy(xpath = "//span[normalize-space()='Create or Edit an Appointment']")
+    public WebElement createOrEditAnAppointmentText;
 
+    @FindBy(xpath = "//input[@id='appointment-id']")
+    public WebElement createAppointmentIdBox;
 
+    @FindBy(xpath = "//input[@id='appointment-startDate']")
+    public WebElement createAppointmentStartDateTime;
 
+    @FindBy(xpath = "//input[@id='appointment-endDate']")
+    public WebElement createAppointmentEndDateTime;
 
+    @FindBy(xpath = "//select[@id='appointment-status']")
+    public WebElement createAppointmentStatus;
 
+    @FindBy(xpath = "//select[@id='appointment-physician']")
+    public WebElement createAppointmentPhysician;
 
+    @FindBy(xpath = "//textarea[@id='appointment-anamnesis']")
+    public WebElement createAppointmentAnamnesisBox;
 
+    @FindBy(xpath = "//textarea[@id='appointment-treatment']")
+    public WebElement createAppointmentTreatmentBox;
 
+    @FindBy(xpath = "//textarea[@id='appointment-diagnosis']")
+    public WebElement createAppointmentDiagnosisBox;
 
+    @FindBy(xpath = "//textarea[@id='appointment-prescription']")
+    public WebElement createAppointmentPrescriptionBox;
+
+    @FindBy(xpath = "//button[@id='save-entity']")
+    public WebElement createAppointmentSaveButton;
+
+    @FindBy(xpath = "//select[@id='appointment-status']")
+    public WebElement statusDropDownElementi;
 
 
 
--- src/test/resources/features/US011.feature
+++ src/test/resources/features/US011.feature
@@ -2,15 +2,51 @@ Feature: US0011_medunna
 
 
 
-    @us11
-    Scenario: TC01_Kullanıcı (Doktor)"Create or Edit an Appointment" sayfasına gelir
+Background: US011
+
     Given kullanici(doktor)"MDNUrl" sayfasına gider
-    And kullanici(doktor)giris ikonunu tiklar
-    Then kullanici(doktor) Sign in i secer
-    And kullanici(doktor)"Username" textbox'a  geçerli bir username girer
-    And kullanici(doktor)"Password" textbox'a geçerli bir password girer
-    Then kullanici(doktor)My Pages menu butonuna tıklar
-    And kullanici(doktor)My Appoinment secenegini seçer
-    Then kullanici(doktor)Edit butonunu tıklar
-    And kullanici(doktor)Create or Edit an Appointment sayfasına gider
+        And kullanici(doktor)giris ikonunu tiklar
+        Then kullanici(doktor) Sign in i secer
+        And kullanici(doktor)"Username" textbox'a  geçerli bir username girer
+        And kullanici(doktor)"Password" textbox'a geçerli bir password girer
+        And kullanici(doktor) sign in butonunu tiklar
+        Then kullanici(doktor)My Pages menu butonuna tıklar
+        And kullanici(doktor)My Appoinment secenegini seçer
+        Then kullanici(doktor)Edit butonunu tiklar
+
+
+
+
+    Scenario: TC01 (Kullanıcı(Doktor)  Yeni bir randevu oluşturulabilmeli veya mevcut bir randevu düzenlenebilmeli)
+        Given kullanici(doktor) "Create or Edit an Appointment" sayfasına gider
+
+
+
+    Scenario: TC02 ("Bir randevu güncellendiğinde; kullanıcı (doktor), hastanın aşağıdaki bilgilerini görmelidir.
+    ""id, start and end date, Status, physician and patient""")
+
+        Given Kullanıcı(Doktor)hastanın id bilgilerini görür
+        And  Kullanıcı(Doktor)hastanın start and end date bilgilerini görür
+        And Kullanıcı(Doktor)hastanın Status bilgilerini görür
+        And Kullanıcı(Doktor)hastanın physician and patient bilgilerini görür
+
+
+    Scenario: TC05 ("Status" doktor tarafından "PENDING, COMPLETED veya CANCELLED" olarak seçilebilmeli)
+
+        Given Kullanıcı(Doktor) Status dropdown elementini PENDING, COMPLETED veya CANCELLED seklinde secer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
