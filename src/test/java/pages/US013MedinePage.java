85af4b61725fc3a5e0497517acf18d954fdd432f
medineAnna <medine.ah>
2022-04-06 22:04:43 +0200

    degisiklik

---
+++ src/test/java/pages/US013MedinePage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class US013MedinePage {
+    public US013MedinePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+}
--- src/test/java/stepdefinitions/UI_StepDefinitions/US_013.java
+++ src/test/java/stepdefinitions/UI_StepDefinitions/US_013.java
@@ -1,4 +1,7 @@
 package stepdefinitions.UI_StepDefinitions;
 
+import pages.US013MedinePage;
+
 public class US_013 {
+    US013MedinePage us013MedinePage=new US013MedinePage();
 }
