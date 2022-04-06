8b1fc86f108ac6c101deb364592aff461f1cec44
ibrkorkut07 <95496005+ibrkorkut07@users.noreply.github.com>
2022-04-06 23:47:34 +0200

    Update RegistrationPage.java

--- RegistrationPage.java
+++ RegistrationPage.java
@@ -20,13 +20,19 @@ public class RegistrationPage {
     @FindBy(xpath = "//input[@id='firstPassword']")
     public WebElement NewPasswordKutusu;
 
+    @FindBy(xpath = "//div[@class='jh-card card']//li[1]")
+    public WebElement color1;
 
+    @FindBy(xpath = "//div[@class='jh-card card']//li[2]")
+    public WebElement color2;
 
+    @FindBy(xpath = "//div[@class='jh-card card']//li[3]")
+    public WebElement color3;
 
+    @FindBy(xpath = "//div[@class='jh-card card']//li[4]")
+    public WebElement color4;
 
-
-
-
-
+    @FindBy(xpath = "//div[@class='jh-card card']//li[5]")
+    public WebElement color5;
 
 }
