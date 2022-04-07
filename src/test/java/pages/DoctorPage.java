e1bcfc666a4dcf6192035ad5e96bc76e2be08fee
Ayşe <aysglgungor35@gmail.com>
2022-04-07 17:07:27 +0300

    0704

--- src/test/java/pages/DoctorPage.java
+++ src/test/java/pages/DoctorPage.java
@@ -64,6 +64,90 @@ public class DoctorPage {
     @FindBy(xpath = "//span[normalize-space()='Save']")
     public WebElement createAppointmentSave;
 
+    @FindBy(xpath = "//span[normalize-space()='My Inpatients']")
+    public WebElement myInpatientsSeceneği;
+
+    @FindBy(xpath = " //span[normalize-space()='In Patients']")
+    public WebElement InPatientsYazisi;
+
+    @FindBy(xpath = "//tbody/tr[1]/td[10]/div[1]/a[1]")
+    public WebElement InPatientEditbutonu;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //@FindBy(xpath = "//*[text()='ID']")
+    //public WebElement DoctorID;
+    @FindBy(xpath = "//*[text()='42862']")
+    public WebElement DoctorId;
+
+    //   @FindBy(xpath = "//*[text()='Start Date']")
+    //   public WebElement StartDate;
+    @FindBy(xpath = "//span[.='01/04/22 16:56']")
+    public WebElement StartDate;
+
+
+    //  @FindBy(xpath = "//*[text()='End Date']")
+    // public WebElement EndDate;
+    @FindBy(xpath = "//span[.='01/04/22 16:56']")
+    public WebElement EndDate;
+
+    @FindBy(xpath = "//*[text()='Status']")
+    public WebElement Status;
+
+    @FindBy(xpath = "//*[text()='Description']")
+    public WebElement Description1;
+
+    @FindBy(xpath = "  (//*[text()='01/04/22 01:21'])[2]")
+    public WebElement CreatedDate;
+
+    @FindBy(xpath = "//*[text()='Room']")
+    public WebElement Room;
+
+    @FindBy(xpath = "//*[text()='Appointment']")
+    public WebElement Appointment;
+
+    @FindBy(xpath = "//*[text()='Patient']")
+    public WebElement Patient;
+
+
+
+    @FindBy(xpath = "//input[@name='id']")
+    public WebElement EditPageID;
+
+    @FindBy(xpath = "//input[@name='startDate']")
+    public WebElement EditPageStartDate;
+
+    @FindBy(xpath = "//input[@name='endDate']")
+    public WebElement EditPageEndDate;
+
+    @FindBy(xpath = "//input[@id='in-patient-description']")
+    public WebElement EditPageDescription;
+
+    @FindBy(xpath = "//input[@id='in-patient-createdDate']")
+    public WebElement EditPageCreatedDate;
+
+    @FindBy(xpath = "//select[@name='status']")
+    public WebElement EditPageStatus;
+
+    @FindBy(xpath = "//select[@name='room.id']")
+    public WebElement EditPageroom;
+
+    @FindBy(id = "save-entity")
+    public WebElement Save;
+
 
 
 
